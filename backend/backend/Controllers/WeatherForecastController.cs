0a6c99e64bfe8250cdcdfe14086189bb1e4af253
Z-Maguinho-do-Piaui <klebermirandalima@gmail.com>
2022-03-11 12:36:46 -0300

    Terei problemas com o Cors

--- backend/backend/Controllers/WeatherForecastController.cs
+++ backend/backend/Controllers/WeatherForecastController.cs
@@ -24,16 +24,20 @@ namespace backend.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Deu tudo certo, porra!";
         }
     }
 }
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,38 @@
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
+import axios from "axios";
+import {useState, useEffect } from "react";
+import { BASE_URL } from "utils/requests"
+import { MoviePage } from "types/movie";
 
 
 function Listing() {
+    
+    
+    
+    const [pageNumber, setPageNumber] = useState(0);
+
+    // Assim ele não bate na api mais de uma vez por conta do useEffect não ter nenhum objeto para ele observar 
+    useEffect(() => {
+        axios.get(`${BASE_URL}/WeatherForecast`)
+        .then(response => {
+            const data = response.data as MoviePage
+            setPageNumber(data.number);
+        });
+    }, []);
+
+    
+
+    // //FORMA ERRADA 
+    //  axios.get(`${BASE_URL}/WeatherForecast`)
+    //     .then(response => {
+    //         console.log(response.data);
+    //     });
+    
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
-
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
